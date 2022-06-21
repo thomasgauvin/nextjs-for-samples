4047b920a0ddf0c617f33b2e9cb12ff51c0c76fe
Vivek Jilla <vivekjilla7@gmail.com>
2022-06-21 16:19:41 +0530

    Update next.config.js

--- examples/headers/next.config.js
+++ examples/headers/next.config.js
@@ -1,3 +1,4 @@
+// next.config.js
 module.exports = {
   async headers() {
     return [
@@ -19,6 +20,33 @@ module.exports = {
           },
         ],
       },
-    ]
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value:
+                "default-src 'self'; font-src 'self' 'https://fonts.googleapis.com'; img-src 'self' *.somewhere.com; script-src 'self'",
+            },
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'origin-when-cross-origin',
+            },
+            {
+              key: 'Permissions-Policy',
+              value: "camera=(); battery=(self); geolocation=(); microphone=('https://somewhere.com')",
+            },
+          ],
+        },
+      ];
   },
-}
+};
+
