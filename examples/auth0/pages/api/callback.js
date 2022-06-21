211629adfb5068871bd108a1717342a03e620e83
Vivek Jilla <vivekjilla7@gmail.com>
2022-06-21 12:03:44 +0530

    Update callback.js

--- examples/auth0/pages/api/callback.js
+++ examples/auth0/pages/api/callback.js
@@ -2,6 +2,7 @@ import auth0 from '../../lib/auth0'
 
 export default async function callback(req, res) {
   try {
+    console.log('req: ', req)
     await auth0.handleCallback(req, res)
   } catch (error) {
     console.error(error)
