22a16498c2564369c902278989735c77bbf04a81
Vivek Jilla <vivekjilla7@gmail.com>
2022-06-21 17:18:09 +0530

    Update logout.js

--- examples/with-passport/pages/api/logout.js
+++ examples/with-passport/pages/api/logout.js
@@ -2,5 +2,6 @@ import { removeTokenCookie } from '../../lib/auth-cookies'
 
 export default async function logout(req, res) {
   removeTokenCookie(res)
+  console.log("removed token")
   res.end()
 }
