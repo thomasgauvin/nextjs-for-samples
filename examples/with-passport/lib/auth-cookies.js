d4b5a46396e89ee4a5048193e364232ed59f35d4
Vivek Jilla <vivekjilla7@gmail.com>
2022-06-28 11:29:49 +0530

    Negative expires time

--- examples/with-passport/lib/auth-cookies.js
+++ examples/with-passport/lib/auth-cookies.js
@@ -20,7 +20,7 @@ export function setTokenCookie(res, token) {
 export function removeTokenCookie(res) {
   const cookie = serialize(TOKEN_NAME, 'dummy', {
     maxAge: MAX_AGE,
-    expires: new Date(Date.now() + MAX_AGE * 1000),
+    expires: new Date(Date.now() - MAX_AGE * 1000),
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     path: '/',
