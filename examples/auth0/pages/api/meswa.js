6a51e6010e844e515cd625943f8aebaeb31b452e
Vivek Jilla <vivekjilla7@gmail.com>
2022-07-09 11:21:57 +0530

    Update meswa.js

--- examples/auth0/pages/api/meswa.js
+++ examples/auth0/pages/api/meswa.js
@@ -1,7 +1,11 @@
 export default async function meswa(req, res) {
  
-  const header = req.headers['x-ms-client-principal'];
-  const encoded = Buffer.from(header, 'base64');
-  const decoded = encoded.toString('ascii');
+   const header = req.headers['x-ms-client-principal'];
+   console.log(header+" is the x-ms-client-principal")
+   const encoded = Buffer.from(header, 'base64');
+   console.log(encoded+" is the encoded")
+   const decoded = encoded.toString('ascii');
+   console.log(decoded+" is the decoded")
+   console.log("json: "+JSON.parse(decoded))
   res.json({ me: JSON.parse(decoded) })
 }
